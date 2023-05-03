b0406d2e256df18609dfdc459202e0ad46690b57
Adam Kecskes <adam.kecskes@philabs.xyz>
2023-05-03 14:56:22 -0500

    chore: add tailwindcss

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,7 +120,18 @@ const config = {
       },
     }),
     plugins: [
-      'docusaurus-plugin-sass', require.resolve("@cmfcmf/docusaurus-search-local")
+      'docusaurus-plugin-sass', require.resolve("@cmfcmf/docusaurus-search-local"),
+      async function myPlugin(context, options) {
+        return {
+          name: "docusaurus-tailwindcss",
+          configurePostCss(postcssOptions) {
+            // Appends TailwindCSS and AutoPrefixer.
+            postcssOptions.plugins.push(require("tailwindcss"));
+            postcssOptions.plugins.push(require("autoprefixer"));
+            return postcssOptions;
+          },
+        };
+      },
     ]
 };
 
