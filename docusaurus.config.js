fa978ac376b4a780b347185f1113b1e962e0371c
Adam Kecskes <adam@kecskes.net>
2023-05-31 17:27:11 -0500

    show more post links in sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,11 +39,8 @@ const config = {
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          blogSidebarTitle: 'Ten most recent posts',
+          blogSidebarCount: 10,
         },
         theme: {
           customCss: [
