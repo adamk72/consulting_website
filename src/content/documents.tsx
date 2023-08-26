7043c23d6403878ac4d5e445c206b0e089e9bf05
Adam Kecskes <adam@kecskes.net>
2023-08-25 19:52:02 -0500

    add copy and paste document

--- src/content/documents.tsx
+++ src/content/documents.tsx
@@ -9,11 +9,24 @@ export const documents: DocumentItem[] = [
         imageAlt: 'Slack logo',
         description: (
             <>
-                In about 20 pages, this PDF encapsulates 25 years worth of my perspectives about business communications, through the lens of Slack. In it, I explore not just the best practices of using business instant messaging tools in a professional and effective way, but also in a manner that will improve both your organization's communication skills and its overall operations. Collaborate better!
+                In this paper, I explore not just the best practices of using business instant messaging tools in a professional and effective way, but also in a manner that will improve both your organization's communication skills and its overall operations. Collaborate better!
             </>
         ),
         linkText: "Truly Professional Instant Messaging",
         linkUrl: "Truly Professional Instant Messaging (with Slack!).pdf",
+    },
+    {
+        title: <>The Curse of Copy-and-Paste Code</>,
+        subtitle: 'How a non-technical person can influence how software is developed, improve code quality, and get to know their software team a little bit better',
+        imageUrl: 'img/curse.png',
+        imageAlt: 'voodoo doll icon',
+        description: (
+            <>
+                Written for the non-technical or non-developer project stakeholder, this paper goes over a dark code practice that you can actually do something about, no coding required.
+            </>
+        ),
+        linkText: "The Curse of Copy and Paste Code",
+        linkUrl: "The Curse of Copy and Paste Code.pdf",
     },
 ];
 
