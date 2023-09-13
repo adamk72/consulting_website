5c35c4651a66c2ead4c5f80b39e8e1d5ac68649e
Adam Kecskes <adam@kecskes.net>
2023-09-13 12:22:55 -0500

    minor adjustments

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/blue_flipped_imbr_icon.png",
+  favicon: "img/flipped_imbr_icon.png",
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "adamk72", // Usually your GitHub org/user name.
